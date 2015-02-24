90d2ee629e1b0d1d750f6fa6157174b1002ef4d3
Kai Inokuchi <kai.inokuchi@smartnews.com>
2015-02-24 22:03:11 +0900

    Set encrypted cookie (Google Id (sub))

--- options.go
+++ options.go
@@ -26,7 +26,7 @@ type Options struct {
 	GoogleAppsDomains       []string      `flag:"google-apps-domain" cfg:"google_apps_domains"`
 	Upstreams               []string      `flag:"upstream" cfg:"upstreams"`
 	SkipAuthRegex           []string      `flag:"skip-auth-regex" cfg:"skip_auth_regex"`
-	TokenCookieSecret       string        `flag:"token-cookie-secret" cfg:"token_cookie_secret" env:"GOOGLE_AUTH_PROXY_TOKEN_COOKIE_SECRET"`
+	EncryptedCookieSecret   string        `flag:"encrypted-cookie-secret" cfg:"encrypted_cookie_secret" env:"GOOGLE_AUTH_PROXY_ENCRYPTED_COOKIE_SECRET"`
 	// internal values that are set after config validation
 	redirectUrl   *url.URL
 	proxyUrls     []*url.URL
@@ -52,8 +52,8 @@ func (o *Options) Validate() error {
 		errors.New("missing setting: cookie-secret")
 	}
 
-	if o.TokenCookieSecret == "" {
-		errors.New("missing setting: token-cookie-secret")
+	if o.EncryptedCookieSecret == "" {
+		errors.New("missing setting: encrypted-cookie-secret")
 	}
 
 	if o.ClientID == "" {
